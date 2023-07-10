41ecb489bf451a279279f307527ce75db1ba7ddd
suraj548 <surajgudaji548@gmail.com>
2023-07-10 23:56:37 +0530

    added print bill feature

--- src/app/components/bill-details/bill-details.component.ts
+++ src/app/components/bill-details/bill-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BillsService } from 'src/app/bills.service';
 import { Weights } from 'src/app/weights';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-bill-details',
@@ -27,12 +29,27 @@ export class BillDetailsComponent implements OnInit {
     this.billService.getOneBill(billNumber).subscribe(
       (billDetails) => {
         this.selectedBillDetails=billDetails
-        console.log(billDetails.objectsArray_1.length)
+        //console.log(billDetails.objectsArray_1.length)
       },
       (error: any) => {
         console.error('Error fetching bill details:', error);
       }
     );
+  }
+
+  generatePDF(){
+    const element = document.getElementById('bill-details');
+    if(element){
+      html2canvas(element).then((canvas)=>{
+        const pdf = new jsPDF.jsPDF();
+        const imgData = canvas.toDataURL('image/png');
+        const imgProps = pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save('bill.pdf');
+      })
+    }
   }
 
 }
